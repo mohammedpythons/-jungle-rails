2c7acd3fe079dfb3615aa257a903d275c2d43bac
mohammedpythons <mohammedshokai99@gmail.com>
2021-08-19 12:52:05 -0400

    add new feature for admin

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
+
   end
+
+
 
   resources :about
 
