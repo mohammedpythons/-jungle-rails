36fcc747bd77e57e1a26763e99c9ca99f299c677
mohammedpythons <mohammedshokai99@gmail.com>
2021-08-25 06:16:10 -0400

    fix a bug in signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
     private
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
